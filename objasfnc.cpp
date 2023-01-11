8e20c8f9667ac01cf4daf7fc05d9d45c4d8b9497
adj <joshiadarsh421@gmail.com>
2023-01-11 15:32:35 +0545

    ran success

---
+++ objasfnc.cpp
@@ -0,0 +1,36 @@
+//WAp to add two time given in mins and hour using pass obj as fnc argument concept 
+#include<iostream>
+using namespace std;
+class time{
+   int hour, minut;
+    public:
+    void get_time(int a ,int b){
+        hour = a;
+        minut = b;
+
+    }
+    void display_time(){
+        cout<<"in hour is "<<"\n"<<hour<<"\n"<<"in minutes is "<<"\n"<<minut<<endl;
+
+
+    }
+void add_time(time t1, time t2)
+    {
+        minut=t1.minut+t2.minut;
+        hour= minut/60;
+           minut = minut% 60;
+
+        hour=hour+t1.hour+t2.hour;
+    }
+};
+int main()
+{
+    time t1,t2,t3;
+    t1.get_time(2,50);
+    t2.get_time(3,55);
+    t3.add_time(t1,t2);
+    t3.display_time();
+
+    
+ return 0;
+}
