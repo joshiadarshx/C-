73042d5b91fd2c3a1c9318faa9c4cbe0915e3259
adj <joshiadarsh421@gmail.com>
2023-01-11 20:36:07 +0545

    sortingwith sort

---
+++ sort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int ar[5];
+    int i;
+    for ( i = 0; i < 5; i++)//taking input
+    {
+      cin>>ar[i];
+      cout<<"\n";
+
+    }
+    //sorting the given array by sort fnc
+    sort(ar ,ar+5);
+
+    for ( i = 0; i < 5; i++)//taking input
+    {
+      cout<<ar[i]<<endl;
+
+    }
+ return 0;
+}
