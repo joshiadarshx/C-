bb962c051c0f285e55de6e348f0e7b5404d6af98
adj <joshiadarsh421@gmail.com>
2023-01-11 15:36:51 +0545

    eroors were shared

--- objasfnc.cpp
+++ objasfnc.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class time{
    int hour, minut;
     public:
-    void get_time(int a ,int b){
+    void get_time(int a ,int b){ // for t1 takes 2 and 50 and pastes to hour and minut 
         hour = a;
         minut = b;
 
@@ -16,19 +16,19 @@ class time{
     }
 void add_time(time t1, time t2)
     {
-        minut=t1.minut+t2.minut;
-        hour= minut/60;
-           minut = minut% 60;
+        minut=t1.minut+t2.minut; // adds minut
+        hour= minut/60;  //changes sminut to hour 
+           minut = minut% 60;   //erros < % ANT BE USED FOT DOUBLE OR float>  and remaining minuts to hour
 
-        hour=hour+t1.hour+t2.hour;
+        hour=hour+t1.hour+t2.hour; //finally adding all hours
     }
 };
 int main()
 {
     time t1,t2,t3;
-    t1.get_time(2,50);
+    t1.get_time(2,50); // directly passes value 2 and 50 to a and b of obj t1 
     t2.get_time(3,55);
-    t3.add_time(t1,t2);
+    t3.add_time(t1,t2);// < fnc arguments as obj should e mentioned>
     t3.display_time();
 
     
