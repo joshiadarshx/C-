5f4f8ac4d5241055b06b82b084d52da9cb8996c5
adj <joshiadarsh421@gmail.com>
2023-01-11 12:40:06 +0545

    inline

---
+++ inline.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class anil{
+    public :
+    inline void show(){
+        cout<<"hello i am adarsh"<<endl;
+    }
+};
+int main()
+{
+    anil a1;
+    a1.show();
+
+ return 0;
+}
