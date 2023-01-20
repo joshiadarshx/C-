6e581345fe5e357c2224d9474f52629359b21374
adj <joshiadarsh421@gmail.com>
2023-01-20 19:05:02 +0545

    USe of static data

---
+++ staticdata.cpp
@@ -0,0 +1,39 @@
+//static data are thoes data member which can be used or  shared by objects in the class.
+#include<iostream>
+using namespace std;
+class countting{
+    static int count;
+    int b;
+    public :
+    void getdata(){
+        count++;
+    }
+    void show(){
+        int i;
+        for (size_t i = 0; i < 3; i++)
+        {
+             cout<<count<<" "<<i<<endl;
+        
+        }
+        
+     
+
+    }
+};
+int countting :: count;
+int main()
+{
+    countting a,b,c;
+    a.show();
+    b.show();
+    c.show();
+     a.getdata();
+
+    c.getdata();
+       a.show();
+           b.getdata();
+    b.show();
+    c.show();
+ return 0;
+}
+//we understood that the value of static data member is always same for every fnc
